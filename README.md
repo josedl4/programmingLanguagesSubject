2baf7eb35941794bfed8eb8b073ddbbf5b676e61
Jose Luis Martín Martín <joseop21@gmail.com>
2017-03-22 14:05:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Programming Languages. 
+
+## __Practice 3.__
+
+#### Compile & Run:
+* _Compile with flex:_ 
+ ```
+ flex -o [name of C file] pr3.l
+ ```
+* _Compile with gcc:_
+ ```
+ gcc -o [name of executable file] pr3.c -lm -lfl
+ ```
+* _Run:_
+ ```
+ ./[name of executable file] < [Test case]
+ ```
---
+++ practice3/Test1.java
@@ -0,0 +1,14 @@
+public class Test1 {
+    /**
+     * Esta es la clase de prueba 1, en esta clase se deben leer los idenficadores,
+     * numero y palabras reservadas.
+     * 
+     * Unicamente los que son reconocidos por Java, es decir se omitiran comentarios como este
+     * o cadenas de texto entre comillas como esta "Hola".
+     * 
+     * Ingnora esta palabras reservadas: int, class, void
+     */
+    public static void main(String args[]){
+        System.out.println("Hello World, ignara estas palabras: class, float, enum");
+    }
+}

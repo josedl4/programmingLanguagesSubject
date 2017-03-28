cc4c4ffe10086d55bba4172e917a17904e143674
Jose Luis Martín Martín <joseop21@gmail.com>
2017-03-28 21:38:53 +0200

    Added minor changes

--- practice3/Test1.java
+++ practice3/Test1.java
@@ -28,12 +28,21 @@ public class Test1 {
         char c11;
         char c12;
 
+        // Otro comentario mas ;)
+
         String chain = "Otra cadena que no se va a leer";
 
         int my_int = 0;
         my_int ++;
 
-        float my_float2 = 1.99E34;
+        int _4;
+        int __;
+        int _r;
+        
+
+        float my_float2 = 1.99f;
         my_float2 *= 3;
+
+        double dbreak = 1.99E34;
     }
 }
